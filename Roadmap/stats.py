4a7ad36a284b289d2b308a0c36f8bbf89580aba8
Dkrovelasco <kroveltechnology@gmail.com>
2025-02-20 14:28:10 -0500

    Create stats.py

---
+++ Roadmap/stats.py
@@ -0,0 +1,8 @@
+# Solicitar la edad del usuario
+edad = int(input("Por favor, ingresa tu edad: "))
+
+# Verificar si es mayor de edad
+if edad >= 18:
+    print("Eres mayor de edad.")
+else:
+    print("No eres mayor de edad.")
